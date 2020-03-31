388185ec4e94696373af3374939c71d65db1a6c8
ziper <ziper02@gmail.com>
2020-03-31 14:07:39 +0300

    fixed json dict

--- Flights/Utilities/EasyJet.py
+++ Flights/Utilities/EasyJet.py
@@ -1,11 +1,8 @@
 import time
 from datetime import datetime
-from pathlib import Path
-
 import requests
 from dateutil.relativedelta import relativedelta
 from tqdm import tqdm
-
 import Moderator
 from DataManager import data_manager
 from Entity.Airport import Airport
@@ -29,13 +26,14 @@ locations = ('Amsterdam', 'London Stansted', 'Berlin Schoenefeld', 'Berlin Tegal
 
 whole_month_url = data_manager.Easyjet_whole_month_request
 
-currency_url = data_manager.currency_conversion.format(src='EUR',dest='ILS')
+currency_url = data_manager.currency_conversion.format(src='EUR', dest='ILS')
 request_currency = requests.get(url=currency_url)
-exchange_rate=request_currency.json()['EUR_ILS']
+exchange_rate =request_currency.json()['EUR_ILS']
+
 
 def export_whole_months_all_dest():
-    depart_list=[Airport(code=o) for o in Moderator.depart_list]
-    destination_list=[Airport(code=o) for o in Moderator.destination_list_easyjet]
+    depart_list = [Airport(code=o) for o in Moderator.depart_list]
+    destination_list = [Airport(code=o) for o in Moderator.destination_list_easyjet]
     for depart in depart_list:
         t_progress_bar_destination = tqdm(destination_list, leave=True)
         for destination in t_progress_bar_destination:
@@ -50,7 +48,8 @@ def export_whole_months(depart=None, destination=None):
     request_whole_month_url = whole_month_url.format(depart=depart.code, destination=destination.code,
                                                      depart_date=date_str)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/80.0.3987.149 Safari/537.36'}
     request = requests.get(url=request_whole_month_url, headers=headers)
     data_depart_hash = request.json()
     request_whole_month_url = whole_month_url.format(depart=destination.code, destination=depart.code,
@@ -59,17 +58,18 @@ def export_whole_months(depart=None, destination=None):
     data_return_hash = request.json()
     data_depart = data_depart_hash["months"]
     data_return = data_return_hash["months"]
-    year_month_day_date_return_str = str(data_return[len(data_return)-1]["year"]) + '-' + month_string_to_number(data_return[len(data_return)-1]["monthDisplayName"])+ '-' \
-                                 + str(len(data_return[len(data_return)-1]['days']))
+    year_month_day_date_return_str = str(data_return[len(data_return) - 1]["year"]) + '-' + month_string_to_number(
+        data_return[len(data_return) - 1]["monthDisplayName"]) + '-' \
+                                     + str(len(data_return[len(data_return) - 1]['days']))
     return_max_date_datetime = datetime.strptime(year_month_day_date_return_str, '%Y-%m-%d')
-    flights=[]
+    flights = []
     for month in data_depart:
         year_month_date_depart = str(month["year"]) + '-' + month_string_to_number(month["monthDisplayName"])
         days = month['days']
         dayIndex = 0
         for day in days:
             if not day['lowestFare'] is None:
-                price_depart=day['lowestFare']
+                price_depart = day['lowestFare']
                 day_depart = ("0" if (dayIndex + 1) < 10 else "") + str(dayIndex + 1)
                 selected_date_depart_str = year_month_date_depart + '-' + day_depart
                 potential_days_return_list = []
@@ -77,20 +77,22 @@ def export_whole_months(depart=None, destination=None):
                 for j in range(3, 7):
                     potential_days_return_list.append(selected_date_depart_datetime + relativedelta(days=j))
                 for potential_day_return in potential_days_return_list:
-                    day_return_str = str(int(potential_day_return.strftime("%d"))-1)
-                    month_return_str = str(int(potential_day_return.strftime("%m"))- int(month_string_to_number(data_return[0]["monthDisplayName"])))
+                    day_return_str = str(int(potential_day_return.strftime("%d")) - 1)
+                    month_return_str = str(int(potential_day_return.strftime("%m")) - int(
+                        month_string_to_number(data_return[0]["monthDisplayName"])))
                     selected_date_return_str = datetime.strftime(potential_day_return, '%Y-%m-%d')
-                    if potential_day_return<return_max_date_datetime:
+                    if potential_day_return < return_max_date_datetime:
                         if not data_return[int(month_return_str)]['days'][int(day_return_str)]['lowestFare'] is None:
-                            price_return=data_return[int(month_return_str)]['days'][int(day_return_str)]['lowestFare']
-                            total_price=exchange_rate*(price_depart+price_return)
-                            flight=Flight(departure=depart,destination=destination,depart_date=selected_date_depart_str,
-                                          return_date=selected_date_return_str,price=total_price,source='Easyjet')
+                            price_return = data_return[int(month_return_str)]['days'][int(day_return_str)]['lowestFare']
+                            total_price = exchange_rate * (price_depart + price_return)
+                            flight = Flight(departure=depart, destination=destination,
+                                            depart_date=selected_date_depart_str,
+                                            return_date=selected_date_return_str, price=total_price, source='Easyjet')
                             flights.append(flight)
             dayIndex = dayIndex + 1
-        General.update_json_files(flights=flights,year_month_date_depart=year_month_date_depart,destination=destination)
+        General.update_json_files(flights=flights, year_month_date_depart=year_month_date_depart,
+                                  destination=destination)
         flights = []
-
 
 
 def create_dict_for_location(st='all'):
@@ -99,8 +101,8 @@ def create_dict_for_location(st='all'):
     :return: dict that the key is location-month-year
     '''
     months = (
-    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
-    'December')
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
+        'December')
     years = (2019, 2020, 2021, 2022, 2023, 2024)
     dict = {}
     if st == 'all':
